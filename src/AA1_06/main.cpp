388d4519990123239186a1f456e3850410280cfd
Victor Blas <victor.blas@enti.cat>
2021-02-03 01:36:50 +0100

    El preorder i tal shasta fet

--- src/AA1_06/main.cpp
+++ src/AA1_06/main.cpp
@@ -6,19 +6,16 @@ int main() {
 	
 
 	BT t1;
-	std::cout << t1.Exist(1) << std::endl;
-	std::cout << t1.Exist(2) << std::endl;
-	std::cout << t1.Exist(3) << std::endl;
-	std::cout << t1.Exist(4) << std::endl;
-	std::cout << t1.Exist(5) << std::endl;
-	std::cout << t1.Exist(6) << std::endl;
-	std::cout << t1.Exist(7) << std::endl;
-	std::cout << t1.Exist(8) << std::endl;
-	std::cout << t1.Exist(9) << std::endl;
+	//Destructor fet
+	//Search + Exist fet               
+	//std::cout << t1.Exist(1) << std::endl;
 
-	/*t1.PreOrder();
 
-	int x = t1.GetNumberNodesI();
+	t1.PreOrder();
+	t1.PostOrder();
+	t1.InOrder();
+
+	/*int x = t1.GetNumberNodesI();
 
 	std::vector<int> v;
 
